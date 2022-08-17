d354b7eb4372678c95e587a834d7fd470f1b6528
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-08-17 11:03:13 +0300

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Global } from 'styles'
 import { App } from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Global />
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
 
 reportWebVitals()
